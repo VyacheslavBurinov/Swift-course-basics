1905910f47742dc4e8ab21fce7f38a078d873907
VyacheslavBurinov <sburinov@icloud.com>
2020-12-22 17:20:54 +0300

    Четное-нечетное

---
+++ 2l_vyacheslav.burinov.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Функция для определения четности числа"
+
+let number = 5
+
+func checkNumber() {
+    if number % 2 == 0 {
+    print("Четное число")
+    } else{
+    print("Нечетное число")
+    }
+}
+checkNumber()
